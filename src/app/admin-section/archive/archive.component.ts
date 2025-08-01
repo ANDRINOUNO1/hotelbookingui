75d059563867a5533daa9710558f1321ea5606ad
unknown <orewaniruuda@gmail.com>
2025-08-01 10:15:23 +0800

    lacking roomno and roomtypes fetching

--- src/app/admin-section/archive/archive.component.ts
+++ src/app/admin-section/archive/archive.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ArchiveService } from '../../_services/archive.service';
+import { ArchiveDataService } from '../../_services/archive.service';
 import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-archive',
@@ -11,7 +11,7 @@ export class ArchiveComponent implements OnInit {
   archives: any[] = [];
   selectedArchive: any = null;
 
-  constructor(private archiveService: ArchiveService) {}
+  constructor(private archiveService: ArchiveDataService) {}
 
   ngOnInit(): void {
     this.archiveService.getAllArchives().subscribe({
--- src/app/superadmin-sect/archives/archives.component.ts
+++ src/app/superadmin-sect/archives/archives.component.ts
@@ -1,11 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ArchiveDataService } from '../../_services/archive.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-archives',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './archives.component.html',
   styleUrl: './archives.component.scss'
 })
 export class ArchivesComponent {
+archives: any[] = [];
+  selectedArchive: any = null;
 
+  constructor(private archiveService: ArchiveDataService) {}
+
+  ngOnInit(): void {
+    this.archiveService.getAllArchives().subscribe({
+      next: data => {
+        this.archives = data;
+      },
+      error: err => console.error('Failed to load archives', err)
+    });
+  }
+
+
+  viewDetails(archive: any) {
+    this.selectedArchive = archive;
+  }
+
+  closeDetails() {
+    this.selectedArchive = null;
+  }
 }
