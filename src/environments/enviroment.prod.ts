117bf955c5e4a730c2e6fc67ff18342bde5ff436
Niel Ivan <orewaniruuda@gmail.com>
2025-08-16 21:38:28 +0800

    fix

--- src/environments/enviroment.prod.ts
+++ src/environments/enviroment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    apiUrl: 'deploymentbackend-production.up.railway.app'
+    apiUrl: 'https://deploymentbackend-production.up.railway.app'
   };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  apiUrl: 'deploymentbackend-production.up.railway.app'
+  apiUrl: 'https://deploymentbackend-production.up.railway.app'
 };
 
 /*
