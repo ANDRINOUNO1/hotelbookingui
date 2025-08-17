dc99304531f1cde84d6ec8d0747af96ad8bd5017
ANDRINOUNO1 <mataandrewczar@gmail.com>
2025-08-17 12:57:59 +0800

    waw

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,8 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: true,
-  apiUrl: 'https://deploymentbackend-production.up.railway.app'
+  production: false,
+  apiUrl: 'http://localhost:4000/api'
 };
 
 /*
