d19a4058fea0e7ed8de27a00560821677f54b851
KazukiMutsiMutsi <apolbanana52@gmail.com>
2025-08-29 17:04:11 +0800

    deployment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: true,
-  apiUrl: 'https://deploymentbackend-production.up.railway.app',
+  apiUrl: 'https://deploymentbackend-production-5546.up.railway.app',
   cloudinary:{
     cloudName: 'dsheuvqdc'
   }
