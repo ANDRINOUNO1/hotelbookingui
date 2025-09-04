7d81565aa3ea20ee37079132c8a00073925486aa
Niel Ivan <orewaniruuda@gmail.com>
2025-09-05 06:17:48 +0800

    edited railway

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: true,
-  apiUrl: 'http://localhost:4000',
+  apiUrl: 'https://deploymentbackend-production-fb99.up.railway.app/',
   cloudinary:{
     cloudName: 'dsheuvqdc'
   }
