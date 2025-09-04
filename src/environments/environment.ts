7d12d5053437777c4964706ad45f2eb0a6f8bd10
Niel Ivan <orewaniruuda@gmail.com>
2025-09-05 06:27:17 +0800

    fix

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: true,
-  apiUrl: 'https://deploymentbackend-production-fb99.up.railway.app/',
+  apiUrl: 'https://deploymentbackend-production-fb99.up.railway.app',
   cloudinary:{
     cloudName: 'dsheuvqdc'
   }
