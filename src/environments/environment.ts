4a7df01ae26e51391192fd9f1acb1216cae4e84e
Jurace Lomutos <juracelacsamana16@gmail.com>
2025-09-05 16:45:16 +0800

    edited header/footer

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: true,
-  apiUrl: 'http://localhost:4000',
+  apiUrl: 'https://deploymentbackend-production-1612.up.railway.app',
   cloudinary:{
     cloudName: 'dsheuvqdc'
   } 
